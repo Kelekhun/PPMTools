4fa39292a8ef7d4e95c9cb85e886d956036e9e17
kelekhun <khadkadeepak@gmail.com>
2019-08-17 03:06:42 -0400

    Added error validation to ProjectController. Closes #7

--- src/main/java/com/kelekhun/ppmtools/controllers/ProjectController.java
+++ src/main/java/com/kelekhun/ppmtools/controllers/ProjectController.java
@@ -1,15 +1,24 @@
 package com.kelekhun.ppmtools.controllers;
 
 import com.kelekhun.ppmtools.domain.Project;
+import com.kelekhun.ppmtools.services.MapValidationErrorService;
 import com.kelekhun.ppmtools.services.ProjectService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/projects/")
 public class ProjectController {
@@ -17,8 +26,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @RequestMapping(method = RequestMethod.POST,path = "")
-    public ResponseEntity<Project> createOrUpdateProject(@RequestBody Project project){
+    public ResponseEntity<?> createOrUpdateProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.getMapValidationErrors(result);
+        if(errorMap!=null){
+            return errorMap;
+        }
         return new ResponseEntity<Project>(projectService.saveOrUpdate(project), HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/com/kelekhun/ppmtools/services/MapValidationErrorService.java
@@ -0,0 +1,25 @@
+package com.kelekhun.ppmtools.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class MapValidationErrorService {
+
+    public ResponseEntity<?> getMapValidationErrors(BindingResult result){
+        if(result.hasErrors()){
+            Map<String, String> erorMap = new HashMap<>();
+            for(FieldError error: result.getFieldErrors()){
+                erorMap.put(error.getField(),error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String, String>>(erorMap, HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+}
