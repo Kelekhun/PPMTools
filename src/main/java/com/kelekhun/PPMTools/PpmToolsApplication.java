d769ea58d6ea1f9677bdf148a15fab7e411898ba
kelekhun <khadkadeepak@gmail.com>
2019-08-16 22:41:45 -0400

    Projeect Skeleton Added

---
+++ src/main/java/com/kelekhun/PPMTools/PpmToolsApplication.java
@@ -0,0 +1,13 @@
+package com.kelekhun.PPMTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelekhun/PPMTools/PpmToolsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kelekhun.PPMTools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpmToolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
