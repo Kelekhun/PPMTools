f589710a4b82dd20261150eb52d0eb20b3cf7d49
kelekhun <khadkadeepak@gmail.com>
2019-08-17 00:16:09 -0400

    Implemented POST Operation for the Project Controller. Closes #4

--- src/main/java/com/kelekhun/ppmtools/controllers/ProjectController.java
+++ src/main/java/com/kelekhun/ppmtools/controllers/ProjectController.java
@@ -1,10 +1,24 @@
 package com.kelekhun.ppmtools.controllers;
 
+import com.kelekhun.ppmtools.domain.Project;
+import com.kelekhun.ppmtools.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/projects")
+@RequestMapping("/api/projects/")
 public class ProjectController {
 
+    @Autowired
+    private ProjectService projectService;
+
+    @RequestMapping(method = RequestMethod.POST,path = "")
+    public ResponseEntity<Project> createOrUpdateProject(@RequestBody Project project){
+        return new ResponseEntity<Project>(projectService.saveOrUpdate(project), HttpStatus.CREATED);
+    }
 }
