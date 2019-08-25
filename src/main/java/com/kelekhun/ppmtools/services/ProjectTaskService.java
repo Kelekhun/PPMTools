149aba95cbaeacd49218eee6272a96a8b397032b
Kelekhun <khadkadeepak@gmail.com>
2019-08-24 23:22:36 -0400

    Merge branch 'feature'
    # Conflicts:
    #	src/main/java/com/kelekhun/ppmtools/controllers/ProjectController.java

--- src/main/java/com/kelekhun/ppmtools/services/ProjectTaskService.java
+++ src/main/java/com/kelekhun/ppmtools/services/ProjectTaskService.java
@@ -1,7 +1,9 @@
 package com.kelekhun.ppmtools.services;
 
 import com.kelekhun.ppmtools.domain.ProjectTask;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ProjectTaskService {
     ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask);
 }
